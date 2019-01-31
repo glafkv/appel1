0b2221be3ffa498078670af27c4c6f8b9071c4ad
Gabi <glafkv@mail.umsl.edu>
2019-01-30 20:48:20 -0600

    Everything is where I want it to be right now. Everything prints where it is supposed to. I just need to figure out the second half of the project.

--- README.txt
+++ README.txt
@@ -9,4 +9,14 @@ How to Run:
 	Type these commands into the command line:
 		make 		//this compiles the project
 		./proj1 -h	//display the program options
+		./proj1 -i <inputfilename> -o <outputfilename> 	//you choose the filenames, you can also
+								//type it in the other way.
+		./proj1 -i <inputfilename> 	//filename is your choice, output file is called output.dat
+		./proj1 -o <outputfilename>	//filename is your choice, input file is called input.dat
+		./proj1		//filenames are both default to input.dat and output.dat
 		make clean	//remove all the files from when compiled
+
+Issues:
+	If the user types in ./proj1 -o output.dat -i input.dat
+		or 	     ./proj1 -o output.dat
+	it will print "Unknown option", but still prints to the files properly.
--- proj1.c
+++ proj1.c
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+	//Declaration of variables
 	FILE *infptr = NULL;
 	FILE *outfptr = NULL;
-	//char *infile = NULL;
 	int v;
 	char filename[100], c;
 	
@@ -28,21 +28,30 @@ int main(int argc, char *argv[])
 				printf("\t whatever file you specify. If you do\n");
 				printf("\t not put a filename, output.dat will be the\n");
 				printf("\t default.\n");
+				printf("\t -i <inputfilename> -o <outputfilename> is also\n");
+				printf("\t an option if you want to choose your own filenames.\n");
+				printf("\t -o <outputfilename> -i <inputfilename> is also\n");
+				printf("\t an option if you want to choose your own filenames.\n");
 				exit(0);
 			
 			//option i takes in an input file. If the user does input a file, set it to what the user input.
-				//case 'o':
-				case 'i':	
+				//If the user specifies both specific input and output files, this will do that
+			case 'i':	
 				if(argc == 5){
+					//This will read in the 2nd argument
 					infptr = fopen(argv[2], "r");
+					//Error if the file isn't there
 					if(infptr == NULL){
 						return EXIT_FAILURE;
 					}
+					//This will write to the 4th argument.
 					outfptr = fopen(argv[4], "w");
+					//Error if the file isn't there
 					if(outfptr == NULL){
 						fclose(infptr);
 						return EXIT_FAILURE;
 					}
+					//Get the information from the read file, then print to the write file
 					c = fgetc(infptr);
 					while(c != EOF){
 						fprintf(outfptr, "%c", c);
@@ -50,26 +59,50 @@ int main(int argc, char *argv[])
 					}
 					
 				}
-		
+				//If the user does not specify an output file, output.dat will be the default
 				else if(argc == 3){
-					
-						infptr = fopen(argv[2], "r");
-						outfptr = fopen("output.dat", "w");
-						c = fgetc(infptr);
-						while(c != EOF){
-							fprintf(outfptr, "%c", c);
-							c = fgetc(infptr);
-						}
+					//This will read in the 2nd argument
+					infptr = fopen(argv[2], "r");
+					//Error if the file isn't there
+					if(infptr == NULL){
+						return EXIT_FAILURE;
 					}
-				
+					//This will default to output.dat as the write file	
+					outfptr = fopen("output.dat", "w");
+					//Error if the file isn't there
+					if(outfptr == NULL){
+						fclose(infptr);
+						return EXIT_FAILURE;
+					}
+					//Get the info from the read, print to the write.
+					c = fgetc(infptr);
+					while(c != EOF){
+						fprintf(outfptr, "%c", c);
+						c = fgetc(infptr);
+					}
+				}
+				//DON'T FORGET TO CLOSE YOUR FILES EVER AGAIN
 				fclose(infptr);
 				fclose(outfptr);		
 				exit(0);
 				
+				//Same deal as i
 				case 'o':
 				if(argc == 5){
+					//This time, the read file is the 4th argument.
 					infptr = fopen(argv[4], "r");
+					//Error
+					if(infptr == NULL){
+						return EXIT_FAILURE;
+					}
+					//The write file is the 2nd argument.	
 					outfptr = fopen(argv[2], "w");
+					//Error
+					if(outfptr == NULL){
+						fclose(infptr);
+						return EXIT_FAILURE;
+					}
+					//Read then write to the file
 					c = fgetc(infptr);	
 					while(c != EOF){
 						fprintf(outfptr, "%c", c);
@@ -77,86 +110,31 @@ int main(int argc, char *argv[])
 					}
 				}
 				else if(argc == 3){
+					//This one autosets the read file to input.dat
 					infptr = fopen("input.dat","r");
+					//Error
+					if(infptr == NULL){
+						return EXIT_FAILURE;
+					}
+					//The output is argument 2
 					outfptr = fopen(argv[2], "w");
+					//Error
+					if(outfptr == NULL){
+						fclose(infptr);
+						return EXIT_FAILURE;
+					}
+					//Read from then write to the file
 					c = fgetc(infptr);
 					while(c != EOF){
 						fprintf(outfptr, "%c", c);
 						c = fgetc(infptr);
 					}
 				}
-
+				//Close out ya files
 				fclose(infptr);
 				fclose(outfptr);
-				
-				//else if(argc == 3){
-					
-				//fclose(fptr);
-				//fclose(out);
-				/*else{
-					out = fopen(argv[2], "w");
-					if(out == NULL){
-						fclose(fptr);
-						return EXIT_FAILURE;
-			//		}
-				c = fgetc(fptr);
-				while(c != EOF){
-					fprintf(out, "%c", c);
-					c = fgetc(fptr);
-			//	}
-				}*/
-				//fclose(fptr);
-				//fclose(out);
-			//	break;
-				
-				/*infile = optarg;
-				if(argc != 3){
-					fptr = fopen("input.dat","r");
-					//fptr = optarg;
-					//out = fopen("output.dat", "w");
-					c = fgetc(fptr);
-					while(c != EOF){
-					//	fprintf(out,"%c", c);
-						printf("%c", c);
-						c = fgetc(fptr);
-					}
-					
-					fclose(fptr);
-				}
-				else {
-					fptr = fopen(argv[2], "r");
-					
-					//fptr = optarg;
-					//out = fopen("tester.out", "w");
-					c = fgetc(fptr);
-					while(c != EOF){
-						printf("%c", c);
-					//	fprintf(out, "%c", c);
-						c = fgetc(fptr);
-					}
-					fclose(fptr);
-				}
-				exit(0);*/	
-			/*case 'o':
-				if(argc != 3)
-				                                                                                   ,k	out = fopen("output.dat","w");
-					c = fgetc(fptr);
-					while(c != EOF){
-						fprintf(out, "%c", c);
-						c = fgetc(fptr);
-					}
-					fclose(out);
-				}
-				else {
-					out = fopen(argv[2], "w");
-					c = fgetc(fptr);
-					while(c != EOF){
-						fprintf(out, "%c", c);
-						c = fgetc(fptr);
-					}
-					fclose(out);
-				}
-				exit(0);*/
+			
+				                                                                             
 				
 	
 			case '?':
@@ -168,9 +146,20 @@ int main(int argc, char *argv[])
 
 		}
 	}
+	//If the user doesn't do any of the above choices, it will completely default.
 	if(argc == 1){
 		infptr = fopen("input.dat", "r");
+		//Error
+		if(infptr == NULL){
+			return EXIT_FAILURE;
+		}
+	
 		outfptr = fopen("output.dat", "w");
+		//Error
+		if(outfptr == NULL){
+			fclose(infptr);
+			return EXIT_FAILURE;
+		}
 		c = fgetc(infptr);
 		while(c != EOF){
 			fprintf(outfptr, "%c", c);
